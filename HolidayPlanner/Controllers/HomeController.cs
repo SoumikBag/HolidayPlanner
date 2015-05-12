b6fc3fa2d6f9d7301a88f9687b603a7b878c6f1f
SoumikBag <soumik.bag@northgate-is.com>
2015-05-12 17:08:49 +0530

    Soumik commit

--- HolidayPlanner/Controllers/HomeController.cs
+++ HolidayPlanner/Controllers/HomeController.cs
@@ -21,10 +21,7 @@ namespace HolidayPlanner.Controllers
         }
         //ended by sandy
 
-
-
-
-
+        
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -123,26 +120,44 @@ namespace HolidayPlanner.Controllers
             //             where p.HotelId == 11
             //             select p).ToList();
 
-            if (clickinfo == "room")
-            {
-                var roominfo = from r in db.Rooms
-                               join h in db.Hotels
-                                   on r.RoomId equals h.RoomId
-                               select new InfoViewModel { Rate = r.Rate, RoomDetails = r.RoomDetails, RoomCapacity = r.RoomCapacity };
-
-                return View("RoomInfo", roominfo);
-            }
-
-            else
-            {
-                var facilityinfo = from f in db.Facilities
+            switch(clickinfo)
+            { 
+                case "room":            
+                {
+                    var roominfo = from r in db.Rooms
                                    join h in db.Hotels
-                                       on f.FId equals h.FId
-                                   select new InfoViewModel { FacilityType = f.FacilitiesType, FacilityDetails = f.FacilitiesDetails };
+                                       on r.RoomId equals h.RoomId
+                                   select new InfoViewModel { Rate = r.Rate, RoomDetails = r.RoomDetails, RoomCapacity = r.RoomCapacity };
 
-                return View("FacilityInfo", facilityinfo);
+                    return View("RoomInfo", roominfo);
+                }
+
+                case "facility":
+                {
+                    var facilityinfo = from f in db.Facilities
+                                       join h0 in db.Hotels
+                                       on f.FId equals h0.FId into f0
+
+                                        from h1 in db.Hotels
+                                                                                                                        
+                                       select new InfoViewModel { FacilityType = f.FacilitiesType, FacilityDetails = f.FacilitiesDetails, FoodDetails = h1.FoodDetails, Policies = h1.HotelPolices};
+
+                    return View("FacilityInfo", facilityinfo);
+                }
+
+                case "review":
+                {
+                    var review = from r in db.Reviews
+                                       join h in db.Hotels
+                                       on r.HotelId equals h.HotelId
+                                       select new InfoViewModel { ReviewDetails = r.ReviewDetails, Rating = r.Rating };
+
+                    return View("ReviewInfo", review);
+                }
 
             }
+            return View("Error");
+
         }
 
 
--- HolidayPlanner/Models/InfoViewModel.cs
+++ HolidayPlanner/Models/InfoViewModel.cs
@@ -8,15 +8,20 @@ namespace HolidayPlanner.Models
 {
     public class InfoViewModel 
     {
+        public string Policies;
+        public string FoodDetails;
         public int? Rate;
         public int? RoomCapacity;
         public string RoomDetails;
         public string FacilityType;
         public string FacilityDetails;
+        public string ReviewDetails;
+        public int? Rating;
 
         public IEnumerable<Hotel> Hotel { get; set; }
         public IEnumerable<Room> Room { get; set; }
         public IEnumerable<Facility> Facility { get; set; }
-
+        public IEnumerable<Review> Review { get; set; }
+        
     }
 }
